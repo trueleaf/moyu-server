ea114c451f5a0ca68ee7388e2090c5d483738f69
shuxiaokai <2581105856@qq.com>
2021-09-01 18:06:32 +0800

    feat: 更新默认返回参数

--- app/model/apidoc/docs/docs.js
+++ app/model/apidoc/docs/docs.js
@@ -158,7 +158,7 @@ module.exports = app => {
                     value: {
                         dataType: {
                             type: String,
-                            enum: ["json"],
+                            enum: ["application/json", "application/x-www-form-urlencoded", "multipart/form-data", "text/plain", "application/xml", "text/html"],
                         },
                         json: [ProperytySchema],
                         text: String,
@@ -171,7 +171,15 @@ module.exports = app => {
                 default: [{
                     title: "成功返回",
                     statusCode: 200,
-                    values: []
+                    value: {
+                        dataType: "application/json",
+                        json: [],
+                        file: {
+                            url: "",
+                            raw: "",
+                        },
+                        text: "",
+                    }
                 }]
             },
             headers: [ProperytySchema], //请求头
