c0ffb7ffb5c4518609508755a185f89f4a0e92b4
shuxiaokai <2581105856@qq.com>
2022-01-27 18:01:01 +0800

    fix: 不对dataType进行校验

--- app/model/apidoc/docs/docs.js
+++ app/model/apidoc/docs/docs.js
@@ -170,7 +170,6 @@ module.exports = app => {
                     value: {
                         dataType: {
                             type: String,
-                            enum: ["", "application/json", "application/x-www-form-urlencoded", "multipart/form-data", "text/plain", "application/xml", "text/html"],
                         },
                         json: [ProperytySchema],
                         text: String,
