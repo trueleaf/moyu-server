2f74a7a4037c1df38052a66715a8ca6870bd2e3d
shuxiaokai <2581105856@qq.com>
2023-04-23 20:38:22 +0800

    refactor: (apidoc) 初始化midway代码

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1682253370069_7840',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
