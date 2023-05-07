ed22ad442151a72335054ad2a4727dd3f4043ce7
shuxiaokai <2581105856@qq.com>
2023-05-07 19:25:32 +0800

    feat: (config) 新增User实体定义

--- src/entity/security/user.ts
+++ src/entity/security/user.ts
@@ -1,57 +1,7 @@
-import { prop } from '@typegoose/typegoose';
-// const userSchema = new Schema({
-//   phone: { //手机号码
-//     type: String
-//   },
-//   loginTimes: { //登录次数
-//     type: Number,
-//     default: 0,
-//   },
-//   lastLogin: { //上次登录时间
-//     type: Date,
-//   },
-//   email: { //邮箱
-//     type: String
-//   },
-//   enable: { //使能
-//     type: Boolean,
-//     default: true
-//   },
-//   roleIds: { //角色id
-//     type: Array,
-//     default: []
-//   },
-//   roleNames: {
-//     type: Array, //角色名称，仅供前端显示方便，避免一次查询,
-//     default: []
-//   },
-//   clientRoutes: { //前端路由，权限管理时候进行修改避免用户登录时多次查询
-//     type: Array,
-//     default: [],
-//   },
-//   clinetMenus: { //前端菜单，权限管理时候进行修改避免用户登录时多次查询
-//     type: Array,
-//     default: []
-//   },
-//   serverRoutes: { //后端路由，权限管理时候进行修改避免用户登录时多次查询
-//     type: Array,
-//     default: []
-//   },
-//   recentVisitProjects: { //最近访问的项目
-//     type: Array,
-//     default: []
-//   },
-//   starProjects: { //收藏项目
-//     type: Array,
-//     default: []
-//   },
-//   couldVisitProjects: { //可以访问的项目列表id集合
-//     type: Array,
-//     default: []
-//   }
-// }, {
-//   timestamps: true,
-// });
+import { modelOptions, prop } from '@typegoose/typegoose';
+@modelOptions({
+  schemaOptions: { timestamps: true, collection: 'security_users' },
+})
 export class User {
   /**
    * 用户名称(登录名称)
@@ -74,8 +24,68 @@ export class User {
   @prop()
   public password: string;
   /**
-  * 手机号码
-  */
-  @prop({default: ""})
+   * 手机号码
+   */
+  @prop({ default: '' })
   public phone: string;
+  /**
+   * 登录次数
+   */
+  @prop({ default: 0 })
+  public loginTimes: number;
+  /**
+   * 上次登录时间
+   */
+  @prop()
+  public lastLogin: Date;
+  /**
+   * 邮箱
+   */
+  @prop()
+  public email: string;
+  /**
+   * 角色id列表
+   */
+  @prop({ type: () => [String] })
+  public roleIds: string[];
+  /**
+   * 角色名称，仅供前端显示方便，避免一次查询
+   */
+  @prop({ type: () => [String] })
+  public roleNames: string[];
+  /**
+   * 前端路由，权限管理时候进行修改避免用户登录时多次查询
+   */
+  @prop({ type: () => [String] })
+  public clientRoutes: string[];
+  /**
+   * 前端菜单，权限管理时候进行修改避免用户登录时多次查询
+   */
+  @prop({ type: () => [String] })
+  public clinetMenus: string[];
+  /**
+   * 后端路由，权限管理时候进行修改避免用户登录时多次查询
+   */
+  @prop({ type: () => [String] })
+  public serverRoutes: string[];
+  /**
+   * 最近访问的项目id集合
+   */
+  @prop({ type: () => [String] })
+  public recentVisitProjects: string[];
+  /**
+   * 收藏的项目id集合
+   */
+  @prop({ type: () => [String] })
+  public starProjects: string[];
+  /**
+   * 允许访问项目的id集合
+   */
+  @prop({ type: () => [String] })
+  public couldVisitProjects: string[];
+  /**
+   * 使能
+   */
+  @prop({ default: true })
+  public enable: boolean;
 }
