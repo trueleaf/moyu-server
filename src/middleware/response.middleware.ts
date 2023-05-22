25f2951c78386e662112c182853c119d77f719a9
shuxiaokai <2581105856@qq.com>
2023-05-22 19:52:55 +0800

    refactor: x新增返回参数中间件

---
+++ src/middleware/response.middleware.ts
@@ -0,0 +1,21 @@
+import { Middleware, IMiddleware } from '@midwayjs/core';
+import { NextFunction, Context } from '@midwayjs/koa';
+
+@Middleware()
+export class ResponseWrapperMiddleware implements IMiddleware<Context, NextFunction> {
+  resolve() {
+    return async (ctx: Context, next: NextFunction) => {
+      const result = await next();
+      // 返回给上一个中间件的结果
+      return {
+        code: 0,
+        msg: '操作成功',
+        data: result
+      };
+    };
+  }
+
+  static getName(): string {
+    return 'responseWrapper';
+  }
+}
