052303a9d6a62bad7398478f4457946cf5b5d92e
shuxiaokai <2581105856@qq.com>
2023-05-28 20:53:06 +0800

    feat: 新增session和默认登录密码

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -7,15 +7,24 @@ import { ClientRoutes } from '../entity/security/client_routes';
 import { ServerRoutes } from '../entity/security/server_routes';
 
 export default {
-  // use for cookie sign key, should change to your own and keep security
   keys: '1683383633312_1890',
+  session: {
+    name: 'apiflow',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 3600 * 1000 * 30,
+      httpOnly: true,
+      sameSite: null,
+    },
+  },
   koa: {
     port: 7001,
   },
   mongoose: {
     dataSource: {
       default: {
-        uri: 'mongodb://localhost:27017/moyu',
+        uri: 'mongodb://localhost:27017/test',
         options: {
           useNewUrlParser: true,
           useUnifiedTopology: true,
@@ -36,5 +45,8 @@ export default {
     SignName: '',
     templateCode: '',
     maxAge: 0,
+  },
+  apiflow: {
+    defaultRegisterPassword: '111111',
   },
 } as MidwayConfig;
