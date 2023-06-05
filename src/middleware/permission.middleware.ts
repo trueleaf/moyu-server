fcecd77bf2a479c22ed789ead6662bc8d7c7ef03
shuxiaokai <2581105856@qq.com>
2023-06-05 22:27:06 +0800

    upd: 更新权限拦截逻辑

--- src/middleware/permission.middleware.ts
+++ src/middleware/permission.middleware.ts
@@ -1,26 +1,27 @@
 import { Middleware, IMiddleware, Config, ALL } from '@midwayjs/core';
 import { NextFunction, Context } from '@midwayjs/koa';
-import { GlobalConfig } from '../types/types';
+import { GlobalConfig, LoginTokenInfo } from '../types/types';
 import * as jwt from 'jsonwebtoken';
 import { InjectEntityModel } from '@midwayjs/typegoose';
 import { ReturnModelType } from '@typegoose/typegoose';
 import { User } from '../entity/security/user';
 import { ResponseWrapper } from '../types/response/common/common';
 import { throwError } from '../utils/utils';
+import { ServerRoutes } from '../entity/security/server_routes';
+import { Role } from '../entity/security/role';
 
 @Middleware()
-export class PermissionMiddleware
-  implements IMiddleware<Context, NextFunction>
-{
+export class PermissionMiddleware implements IMiddleware<Context, NextFunction> {
   @Config(ALL)
-  config: GlobalConfig;
+    config: GlobalConfig;
   @InjectEntityModel(User)
-  userModel: ReturnModelType<typeof User>;
-
+    userModel: ReturnModelType<typeof User>;
+  @InjectEntityModel(Role)
+    roleModel: ReturnModelType<typeof Role>;
+  @InjectEntityModel(ServerRoutes)
+    serverRoutesModel: ReturnModelType<typeof ServerRoutes>;
   resolve() {
     return async (ctx: Context, next: NextFunction) => {
-      // const urlWithoutQueryParams = ctx.request.url.replace(/\?.*$/g, '');
-      // let serverRoutes = []; //用户所拥有得权限列表
       try {
         if (!ctx.headers.authorization) {
           return throwError(5000, '缺少Authorization认证头');
@@ -28,42 +29,47 @@ export class PermissionMiddleware
         const tokenInfo = jwt.verify(
           ctx.headers.authorization,
           this.config.jwtConfig.secretOrPrivateKey
+        ) as LoginTokenInfo;
+        ctx.tokenInfo = tokenInfo;
+        const urlWithoutQueryParams = ctx.request.url.replace(/\?.*$/g, '');
+        const serverRouteInfoList: Pick<ServerRoutes, 'path' | 'method'>[] = []; //用户所拥有得权限列表
+        const loginName = tokenInfo.loginName;
+        const userInfo = await this.userModel.findOne({ loginName });
+        const { roleIds } = userInfo;
+        const allServerRoutes = await this.serverRoutesModel.find(
+          {},
+          { path: 1, method: 1 }
         );
-        ctx.userInfo = tokenInfo;
-        // const loginName = tokenInfo.loginName;
-        // const userInfo = await this.userModel.findOne({ loginName });
-        // const allServerRoutes = await ctx.model.Security.ServerRoutes.find(
-        //   {},
-        //   { path: 1, method: 1 }
-        // );
-        // const roleIds = userInfo.roleIds;
-
-        // for (let i = 0; i < roleIds.length; i++) {
-        //   const roleInfo = await ctx.model.Security.Role.findById({
-        //     _id: roleIds[i],
-        //   });
-        //   if (roleInfo) {
-        //     const serverRoutesPathes = roleInfo.serverRoutes.map(val => {
-        //       return allServerRoutes.find(val2 => {
-        //         return val2._id.toString() === val;
-        //       });
-        //     });
-        //     serverRoutes = serverRoutes.concat(serverRoutesPathes);
-        //   }
-        // }
-        // serverRoutes = ctx.helper.unique(serverRoutes, 'id');
-        // const reqMethod = ctx.request.method.toLowerCase();
-        // const hasPermission = serverRoutes.find(val => {
-        //   return (
-        //     val.method.toLowerCase() === reqMethod &&
-        //     val.path === urlWithoutQueryParams
-        //   );
-        // });
-        // if (!hasPermission) {
-        //   ctx.helper.throwCustomError('暂无权限', 4002);
-        // }
+        for (let i = 0; i < roleIds.length; i++) {
+          const roleInfo = await this.roleModel.findById({
+            _id: roleIds[i],
+          });
+          if (roleInfo) {
+            roleInfo.serverRoutes.forEach(routeId => {
+              const matchedRoute = allServerRoutes.find(routeInfo => {
+                return routeInfo.id === routeId;
+              });
+              if (matchedRoute) {
+                serverRouteInfoList.push({
+                  path: matchedRoute.path,
+                  method: matchedRoute.method,
+                });
+              }
+            });
+          }
+        }
+        const reqMethod = ctx.request.method.toLowerCase();
+        const hasPermission = serverRouteInfoList.find(routeInfo => {
+          const isSameMethod = routeInfo.method.toLowerCase() === reqMethod;
+          const isSamePath = routeInfo.path === urlWithoutQueryParams
+          return isSameMethod && isSamePath;
+        });
+        if (!hasPermission) {
+          return throwError(4002, '暂无权限')
+        }
         await next();
       } catch (error: unknown) {
+        ctx.logger.error(error);
         return {
           code: 5000,
           msg: (error as Error).message,
