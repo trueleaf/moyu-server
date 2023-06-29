a883db2f152c9d7bb21740e22dfe51f7859ddebe
shuxiaokai <2581105856@qq.com>
2023-06-29 22:16:58 +0800

    feat: 新增角色service

---
+++ src/service/security/role.ts
@@ -0,0 +1,105 @@
+import { Provide } from '@midwayjs/core';
+import { InjectEntityModel } from '@midwayjs/typegoose';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { throwError } from '../../utils/utils';
+import { Role } from '../../entity/security/role';
+import { AddRoleDto, DeleteRoleDto, EditRoleDto, GetRoleInfoDto, GetRoleListDto } from '../../types/dto/security/role.dto';
+import { TableResponseWrapper } from '../../types/response/common/common';
+
+
+@Provide()
+export class RoleService {
+  @InjectEntityModel(Role)
+    roleModel: ReturnModelType<typeof Role>;
+  /**
+   * 新增角色
+   */
+  async addRole(params: AddRoleDto) {
+    const { roleName, clientRoutes, clientBanner, serverRoutes, remark } = params;
+    const doc: Partial<Role> = {};
+    doc.roleName = roleName;
+    doc.clientRoutes = clientRoutes;
+    doc.clientBanner = clientBanner;
+    doc.serverRoutes = serverRoutes;
+    doc.remark = remark;
+    const hasRole = await this.roleModel.findOne({ roleName });
+    if (hasRole) {
+      throwError(1003, '角色名称已经存在')
+    }
+    await this.roleModel.create(doc);
+    return;
+  }
+  /**
+   * 修改角色
+   */
+  async editRole(params: EditRoleDto) {
+    const { _id, roleName, clientRoutes, clientBanner, serverRoutes, remark } = params;
+    const updateDoc: Partial<Role> = {};
+    updateDoc.roleName = roleName;
+    updateDoc.clientRoutes = clientRoutes;
+    updateDoc.clientBanner = clientBanner;
+    updateDoc.serverRoutes = serverRoutes;
+    updateDoc.remark = remark;
+    await this.roleModel.findByIdAndUpdate({ _id }, updateDoc);
+    return;
+  }
+  /**
+   * 删除角色
+   */
+  async deleteRole(params: DeleteRoleDto) {
+    const { ids } = params;
+    const result = await this.roleModel.updateMany({ _id: { $in: ids }}, { $set: { enabled: false }});
+    return result;
+  }
+  /**
+   * 分页方式获取角色列表
+   */
+  async getRoleList(params: GetRoleListDto) {
+    const { pageNum, pageSize, startTime, endTime } = params;
+    const query = { enabled: true } as {
+      enabled: boolean;
+      createdAt?: {
+        $gt?: number,
+        $lt?: number,
+      };
+    };
+    let skipNum = 0;
+    let limit = 100;
+    if (pageSize != null && pageNum != null) {
+      skipNum = (pageNum - 1) * pageSize;
+      limit = pageSize;
+    }
+    if (startTime != null && endTime != null) {
+      query.createdAt = { $gt: startTime, $lt: endTime };
+    }
+    const rows = await this.roleModel.find(query, { clientRoutes: 0, clientBanner: 0, serverRoutes: 0 }).sort({ updatedAt: -1 }).skip(skipNum).limit(limit);
+    const total = await this.roleModel.find(query).countDocuments();
+    const result: TableResponseWrapper = {
+      rows: [],
+      total: 0
+    };
+    result.rows = rows;
+    result.total = total;
+    return result;
+  }
+  /**
+   * 以枚举方式获取角色列表
+   */
+  async getRoleEnum() {
+    const limit = 100;
+    const result = await this.roleModel.find({ enabled: true }, { roleName: 1 }).limit(limit);
+    return result;
+  }
+  /**
+   * 根据id获取角色信息
+   */
+  async getRoleInfoById(params: GetRoleInfoDto) {
+    const { _id } = params;
+    const query = {
+      _id,
+      enabled: true
+    };
+    const result = await this.roleModel.findOne(query, { createdAt: 0, updatedAt: 0, enabled: 0 });
+    return result;
+  }
+}
