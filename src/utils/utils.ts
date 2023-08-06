74da639e8bdaf65b3b69aa233d9547bd5c8ae4b7
shuxiaokai <2581105856@qq.com>
2023-08-06 23:07:41 +0800

    feat: 新增数据转换方法

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -26,3 +26,52 @@ export const throwError = (code: ResponseWrapper['code'], msg: string) => {
   });
   throw errorData;
 };
+/**
+ * 将数组转换为树结构
+ */
+export const convertPlainArrayDataToTreeData = (plainArray: any[], options?: { id: string, pid: string }) => {
+  const id = options?.id || '_id';
+  const pid = options?.pid || 'pid';
+  const temp = new Map();
+  const result: { children: [] }[] = [];
+  plainArray.forEach(item => {
+    if (!item.children) {
+      item.children = [];
+    }
+    temp.set(item[id].toString(), item)
+  })
+  plainArray.forEach(item => {
+    if (!item[pid]) { //根元素
+      result.push(item);
+    } else {
+      const parent = temp.get(item.pid);
+      parent.children.push(item)
+    }
+  })
+  return result;
+};
+/**
+ * 遍历树形数据
+ */
+
+export const dfsForest = <T extends { children: T[], [propsName: string]: unknown }>(forestData: T[], fn: (item: T, level: number) => void) => {
+  if (!Array.isArray(forestData)) {
+    throw new Error('第一个参数必须为数组类型');
+  }
+  const foo = (forestData: T[], hook: (item: T, level: number) => void, level: number) => {
+    for (let i = 0; i < forestData.length; i += 1) {
+      const currentData = forestData[i];
+      hook(currentData, level);
+      if (!currentData['children']) {
+        continue;
+      }
+      if (!Array.isArray(currentData['children'])) {
+        continue;
+      }
+      if ((currentData['children']).length > 0) {
+        foo(currentData['children'], hook, level + 1);
+      }
+    }
+  };
+  foo(forestData, fn, 1);
+}
