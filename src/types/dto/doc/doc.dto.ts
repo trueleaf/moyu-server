9bedf461b7e0bfa4d6bf5de6bdbc681f3dab82a8
舒小凯 <2581105856@qq.com>
2025-04-23 18:42:29 +0800

    feat: 增加CommonHeaderItem类，优化DocInfo结构

--- src/types/dto/doc/doc.dto.ts
+++ src/types/dto/doc/doc.dto.ts
@@ -6,40 +6,69 @@ import { RequestMethod } from '../../types.js';
 | 通用校验
 |--------------------------------------------------------------------------
 */
-//基础请求参数元信息
-class BaseProperty {
+class CommonHeaderItem {
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
   /**
    * 字段名称
    */
   @Rule(RuleType.string().required().allow(''))
-    key: string;
+  key: string;
   /**
-   * 字段类型
-   */
-  @Rule(RuleType.string().valid('string', 'number', 'boolean', 'array', 'object', 'file').required())
-    type: 'string' | 'number' | 'boolean' | 'array' | 'object' | 'file';
-  /**
-   * 字段描述
-   */
-  @Rule(RuleType.string().required().allow(''))
-    description: string;
+ * 字段描述
+ */
+  @Rule(RuleType.string().allow(''))
+  description?: string;
   /**
    * 字段值
    */
   @Rule(RuleType.string().required().allow(''))
-    value: string;
+  value: string;
+  /**
+* 业务参数，是否选中
+*/
+  @Rule(RuleType.boolean())
+  select?: boolean;
+  /**
+ * 字段类型
+ */
+  @Rule(RuleType.string())
+  type?: string;
+}
+//基础请求参数元信息
+class BaseProperty {
+  @Rule(RuleType.string().required())
+  _id: string;
+  /**
+   * 字段名称
+   */
+  @Rule(RuleType.string().required().allow(''))
+  key: string;
+  /**
+   * 字段类型
+   */
+  @Rule(RuleType.string().valid('string', 'number', 'boolean', 'array', 'object', 'file').required())
+  type: 'string' | 'number' | 'boolean' | 'array' | 'object' | 'file';
+  /**
+   * 字段描述
+   */
+  @Rule(RuleType.string().required().allow(''))
+  description: string;
+  /**
+   * 字段值
+   */
+  @Rule(RuleType.string().required().allow(''))
+  value: string;
   /**
    * 是否必填
    */
   @Rule(RuleType.boolean().required())
-    required: boolean;
+  required: boolean;
   /**
    * 业务参数，是否选中
    */
   @Rule(RuleType.boolean())
-    select?: boolean;
+  select?: boolean;
   /**
    * 业务参数，文件变量类型
    */
@@ -48,32 +77,32 @@ class BaseProperty {
 }
 class MockImage {
   @Rule(RuleType.valid('png', 'jpg', 'gif', 'svg').required())
-    type: 'png' | 'jpg' | 'gif' | 'svg';
+  type: 'png' | 'jpg' | 'gif' | 'svg';
   @Rule(RuleType.number())
-    width: number;
+  width: number;
   @Rule(RuleType.number())
-    height: number;
+  height: number;
   /**
    * 图片额外大小,可以mock较大数据图片来测试网络延迟
    */
   @Rule(RuleType.number().required())
-    size: number;
+  size: number;
   @Rule(RuleType.number().required())
-    fontSize: number;
+  fontSize: number;
   @Rule(RuleType.string().required().allow(''))
-    color: string;
+  color: string;
   @Rule(RuleType.string().required().allow(''))
-    backgroundColor: string;
+  backgroundColor: string;
 }
 
 class MockFile {
   @Rule(RuleType.valid('doc', 'docx', 'xls', 'xlsx', 'pdf', 'zip', 'custom'))
-    type: 'doc' | 'docx' | 'xls' | 'xlsx' | 'pdf' | 'zip' | 'custom';
+  type: 'doc' | 'docx' | 'xls' | 'xlsx' | 'pdf' | 'zip' | 'custom';
   /**
    * 自定义mock文件路径
    */
   @Rule(RuleType.string().allow(''))
-    filePath: string;
+  filePath: string;
 }
 
 class MockInfo {
@@ -81,52 +110,52 @@ class MockInfo {
    * mock地址
    */
   @Rule(RuleType.string().required().allow(''))
-    path: string;
+  path: string;
   /**
    * http状态码
    */
   @Rule(RuleType.number().required())
-    httpStatusCode: number;
+  httpStatusCode: number;
   /**
    * 自定义返回头
    */
   @Rule(RuleType.array().items(getSchema(BaseProperty)).required())
-    responseHeaders: BaseProperty[];
+  responseHeaders: BaseProperty[];
   /**
    * 返回延时
    */
   @Rule(RuleType.number().required())
-    responseDelay: number;
+  responseDelay: number;
   /**
    * 返回数据类型
    */
   @Rule(RuleType.valid('json', 'image', 'file', 'text', 'customJson').required())
-    responseType: 'json' | 'image' | 'file' | 'text' | 'customJson';
+  responseType: 'json' | 'image' | 'file' | 'text' | 'customJson';
   /**
    * 返回json数据
    */
   @Rule(RuleType.string().required().allow(''))
-    json: string;
+  json: string;
   /**
    * 图片返回
    */
   @Rule(getSchema(MockImage).required())
-    image: MockImage;
+  image: MockImage;
   /**
    * 文件相关数据
    */
   @Rule(getSchema(MockFile).required())
-    file: MockFile;
+  file: MockFile;
   /**
    * 纯文本，html，css等
    */
   @Rule(RuleType.string().required().allow(''))
-    text: string;
+  text: string;
   /**
    * 自定义json返回
    */
   @Rule(RuleType.string().required().allow(''))
-    customResponseScript: string;
+  customResponseScript: string;
 }
 
 //文档基本信息
@@ -135,48 +164,48 @@ class DocBaseInfo {
    * 文档名称
    */
   @Rule(RuleType.string().required())
-    name: string;
+  name: string;
   /**
    * 文档描述
    */
   @Rule(RuleType.string().allow(''))
-    description: string;
+  description: string;
   /**
    * 文档版本信息
    */
   @Rule(RuleType.string().empty('').default('1.0'))
-    version: string;
+  version: string;
   /**
    * 文档类型,   1.文件夹 2.普通文档 3.markdown文档
    */
   @Rule(RuleType.string().valid('folder', 'api', 'markdown').required())
-    type: 'folder' | 'api' | 'markdown';
+  type: 'folder' | 'api' | 'markdown';
   /**
    * 创建者
    */
   @Rule(RuleType.string())
-    creator: string;
+  creator: string;
   /**
    * 维护人员，最近一次更新人员
    */
   @Rule(RuleType.string().empty(''))
-    maintainer: string;
+  maintainer: string;
   /**
    * 删除文档的人
    */
   @Rule(RuleType.string().empty(''))
-    deletePerson: string;
+  deletePerson: string;
   /**
    * 录入接口花费时间
    */
   @Rule(RuleType.number())
-    spendTime: number;
+  spendTime: number;
 }
 class FileInfo {
   @Rule(RuleType.string().allow(''))
-    url: string;
+  url: string;
   @Rule(RuleType.string().allow(''))
-    raw: string;
+  raw: string;
 }
 class BinaryValue {
   /**
@@ -188,12 +217,12 @@ class BinaryValue {
    * 文件唯一id，文件将会统一存放服务端，使用这个id来索引文件
    */
   @Rule(RuleType.string().allow(''))
-    id: string;
+  id: string;
   /**
    * 如果附件小于配置的大小，直接转换为uint8数组返回
    */
   @Rule(RuleType.string().allow(''))
-    raw: string;
+  raw: string;
 }
 // 定义一个名为 BinaryInfo 的类，用于存储二进制文件的信息
 class BinaryInfo {
@@ -210,26 +239,26 @@ class BinaryInfo {
 }
 class ResonseValue {
   @Rule(RuleType.string().allow(''))
-    dataType: string;
+  dataType: string;
   @Rule(RuleType.string().allow(''))
-    strJson: string;
+  strJson: string;
   @Rule(RuleType.string().allow(''))
-    text: string;
+  text: string;
   @Rule(getSchema(FileInfo))
-    file: FileInfo;
+  file: FileInfo;
 }
 //返回参数
 class ResponseParams {
   @Rule(RuleType.string())
-    _id: string;
+  _id: string;
   @Rule(RuleType.boolean())
-    isMock?: boolean;
+  isMock?: boolean;
   @Rule(RuleType.string())
-    title: string;
+  title: string;
   @Rule(RuleType.number().allow(200))
-    statusCode: number;
+  statusCode: number;
   @Rule(getSchema(ResonseValue))
-    value: ResonseValue;
+  value: ResonseValue;
 }
 //请求脚本信息
 class RequestScript {
@@ -237,7 +266,7 @@ class RequestScript {
    * 请求脚本信息
    */
   @Rule(RuleType.string().allow(''))
-    raw: string;
+  raw: string;
 }
 class RequestUrl {
   /**
@@ -325,7 +354,7 @@ class ItemInfo {
    * 返回参数
    */
   @Rule(RuleType.array().items(getSchema(ResponseParams)))
-    responseParams: ResponseParams[];
+  responseParams: ResponseParams[];
   /**
    * 请求头
    */
@@ -342,64 +371,150 @@ export class DocInfo {
    * 文档id
    */
   @Rule(RuleType.string())
-    _id: string;
+  _id: string;
   /**
    * 父元素id
    */
   @Rule(RuleType.string())
-    pid: string;
+  pid: string;
   /**
    * 项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
   /**
    * 是否为文件夹
    */
   @Rule(RuleType.boolean().required())
-    isFolder: boolean;
+  isFolder: boolean;
   /**
    * 排序字段，时间戳
    */
   @Rule(RuleType.number().required().default(Date.now()))
-    sort: number;
+  sort: number;
   /**
    * 文档基本信息
    */
   @Rule(getSchema(DocBaseInfo))
-    info: DocBaseInfo;
+  info: DocBaseInfo;
   /**
    * 前置脚本信息
    */
   @Rule(getSchema(RequestScript))
-    preRequest: RequestScript;
+  preRequest: RequestScript;
   /**
    * 后置脚本信息
    */
   @Rule(getSchema(RequestScript))
-    afterRequest: RequestScript;
+  afterRequest: RequestScript;
   /**
    * 公共请求头
    */
-  @Rule(RuleType.array().items(getSchema(BaseProperty)))
-    commonHeaders: BaseProperty[];
+  @Rule(RuleType.array().items(getSchema(CommonHeaderItem)))
+  commonHeaders: CommonHeaderItem[];
   /**
    * 接口相关元素
    */
   @Rule(getSchema(ItemInfo))
-    item: ItemInfo;
+  item: ItemInfo;
   /**
    * 返回参数
    */
   @Rule(RuleType.array().items(getSchema(ResponseParams)))
-    responseParams: ResponseParams[];
+  responseParams: ResponseParams[];
   /**
    * mock信息
   */
   @Rule(getSchema(MockInfo))
-    mockInfo: MockInfo;
+  mockInfo: MockInfo;
 }
-
+export class ImportedDocInfo {
+  /**
+   * 文档id
+   */
+  @Rule(RuleType.string())
+  _id: string;
+  /**
+   * 版本信息
+   */
+  @Rule(RuleType.number())
+  __v: number;
+  /**
+   * 父元素id
+   */
+  @Rule(RuleType.string())
+  pid: string;
+  /**
+   * 项目id
+   */
+  @Rule(RuleType.string().required())
+  projectId: string;
+  /**
+   * 是否为文件夹
+   */
+  @Rule(RuleType.boolean().required())
+  isFolder: boolean;
+  /**
+   * 排序字段，时间戳
+   */
+  @Rule(RuleType.number().required().default(Date.now()))
+  sort: number;
+  /**
+   * 文档基本信息
+   */
+  @Rule(getSchema(DocBaseInfo))
+  info: DocBaseInfo;
+  /**
+   * 前置脚本信息
+   */
+  @Rule(getSchema(RequestScript))
+  preRequest: RequestScript;
+  /**
+   * 后置脚本信息
+   */
+  @Rule(getSchema(RequestScript))
+  afterRequest: RequestScript;
+  /**
+   * 公共请求头
+   */
+  @Rule(RuleType.array().items(getSchema(CommonHeaderItem)))
+  commonHeaders: CommonHeaderItem[];
+  /**
+   * 接口相关元素
+   */
+  @Rule(getSchema(ItemInfo))
+  item: ItemInfo;
+  /**
+   * 返回参数
+   */
+  @Rule(RuleType.array().items(getSchema(ResponseParams)))
+  responseParams: ResponseParams[];
+  /**
+   * mock信息
+  */
+  @Rule(getSchema(MockInfo))
+  mockInfo: MockInfo;
+  /**
+   * 创建时间
+   */
+  @Rule(RuleType.string())
+  createdAt: string;
+  /**
+   * 更新时间
+   */
+  @Rule(RuleType.string())
+  updatedAt: string;
+  /**
+   * 是否删除
+   */
+  @Rule(RuleType.boolean())
+  isEnabled: boolean;
+  /**
+   * 子元素
+   */
+  @Rule(RuleType.array().items(RuleType.link('...')).optional())
+  children?: ImportedDocInfo[];
+}
 /*
 |--------------------------------------------------------------------------
 | DTO
@@ -414,22 +529,22 @@ export class AddEmptyDocDto {
    * 文档名称
    */
   @Rule(RuleType.string().required())
-    name: string;
+  name: string;
   /**
    * 文档类型
    */
   @Rule(RuleType.string().valid('folder', 'api', 'markdown').required())
-    type: 'folder' | 'api' | 'markdown';
+  type: 'folder' | 'api' | 'markdown';
   /**
    * 父元素id
    */
   @Rule(RuleType.string().empty(''))
-    pid: string;
+  pid: string;
   /**
    * 所属项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
 }
 
 /**
@@ -440,16 +555,16 @@ export class GenerateDocCopyDto {
    * 节点id
    */
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
   /**
    * 项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
 }
 class IdMap {
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
 }
 /**
  * 粘贴文档
@@ -459,22 +574,22 @@ export class PasteDocsDto {
    * 当前项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
   /**
    * 来源项目id
    */
   @Rule(RuleType.string().required())
-    fromProjectId: string;
+  fromProjectId: string;
   /**
    * 挂载节点id
    */
   @Rule(RuleType.string())
-    mountedId: string;
+  mountedId: string;
   /**
    * docs
    */
   @Rule(RuleType.array().items(getSchema(IdMap)).required())
-    docs: IdMap[];
+  docs: IdMap[];
 }
 /**
  * 改变文档位置信息 todo 迁移前用于历史记录
@@ -511,22 +626,22 @@ export class ChangeDocPositionDto {
    * 当前项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
   /**
    * 需要改变的文档id
    */
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
   /**
    * 需要改变的文档父级id
    */
   @Rule(RuleType.string().allow(''))
-    pid: string;
+  pid: string;
   /**
    * 文档排序
    */
   @Rule(RuleType.number())
-    sort: number;
+  sort: number;
 }
 /**
  * 改变文档基本信息
@@ -536,17 +651,17 @@ export class ChangeDocBaseInfoDto {
    * 文档id
    */
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
   /**
    * 文档所属项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
   /**
    * 文档名称
    */
   @Rule(RuleType.string())
-    name?: number;
+  name?: number;
 }
 /**
  * 更新文档
@@ -556,42 +671,42 @@ export class UpdateDoc {
    * 文档id
    */
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
   /**
    * 项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
   /**
    * 录入耗时
    */
   @Rule(RuleType.number())
-    spendTime: number;
+  spendTime: number;
   /**
    * 文档信息
    */
   @Rule(getSchema(DocBaseInfo).required())
-    info: DocBaseInfo;
+  info: DocBaseInfo;
   /**
    * 接口信息
    */
   @Rule(getSchema(ItemInfo).required())
-    item: ItemInfo;
+  item: ItemInfo;
   /**
    * 前置脚本信息
    */
   @Rule(getSchema(RequestScript).required())
-    preRequest: RequestScript;
+  preRequest: RequestScript;
   /**
    * 后置脚本信息
    */
   @Rule(getSchema(RequestScript).required())
-    afterRequest: RequestScript;
+  afterRequest: RequestScript;
   /**
    * mock信息
    */
   @Rule(getSchema(MockInfo))
-    mockInfo: MockInfo;
+  mockInfo: MockInfo;
 }
 
 /**
@@ -602,7 +717,7 @@ export class CreateDocDto {
    * 文档
    */
   @Rule(getSchema(DocInfo))
-    docInfo: DocInfo;
+  docInfo: DocInfo;
 }
 
 /**
@@ -613,12 +728,12 @@ export class GetDocDetailDto {
    * 文档id
    */
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
   /**
    * 项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
 }
 /**
  * 删除文档
@@ -628,12 +743,12 @@ export class DeleteDocDto {
    * 需要删除文档的id集合
    */
   @Rule(RuleType.array().items(RuleType.string()).required())
-    ids: string;
+  ids: string;
   /**
    * 项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
 }
 
 /**
@@ -644,7 +759,7 @@ export class GetMockDataDto {
    * 文档id
    */
   @Rule(RuleType.string().required())
-    _id: string;
+  _id: string;
 }
 /**
  * 以树形结构获取文档
@@ -654,5 +769,5 @@ export class GetDocsAsTreeDto {
    * 项目id
    */
   @Rule(RuleType.string().required())
-    projectId: string;
+  projectId: string;
 }
